18adadf0dc6908b1aef80f44bb08aa4a85438584
Ignacio Gonzalez Chemes <nachogonza123@gmail.com>
2023-01-04 07:07:13 -0300

    Add Files services

--- src/Routes/ServerRoutes.go
+++ src/Routes/ServerRoutes.go
@@ -3,10 +3,21 @@ package routes
 import (
 	"net/http"
 
-	server_services "github.com/ignaciochemes/beammp-server-info-api/src/Services"
+	services "github.com/ignaciochemes/beammp-server-info-api/src/Services"
 )
 
 func GetDataFromBMPBackendRoute(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	server_services.GetDataFromBMPBackendService(w, r)
+	services.GetDataFromBMPBackendService(w, r)
+}
+
+func GetNewVersionOfServer(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	services.GetNewVersionOfServer()
+}
+
+func SendBeamMPServerFiles(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/zip")
+	w.Header().Set("Content-Disposition", "attachment; filename=BeamMP_Server.zip")
+	services.SendBeamMPServerFilesService(w, r)
 }
